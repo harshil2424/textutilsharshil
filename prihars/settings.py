587659fda57c071dbd63648b4639341856fad1d8
harshil <73432873+harshil2424@users.noreply.github.com>
2021-02-19 14:15:33 +0530

    Update settings.py

--- prihars/settings.py
+++ prihars/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '(-54%x%&#5ykv*l2=_$=7otvwuz(g1!iby!o2tq5m^)v4yt#^k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['prihars.gq']
 
